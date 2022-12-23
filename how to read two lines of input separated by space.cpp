a0a3fc7af9e361885233dcb411e925c1c734cae0
lewws66 <119733039+lewws66@users.noreply.github.com>
2022-12-23 11:49:02 +0800

    Update how to read two lines of input separated by space.cpp

--- how to read two lines of input separated by space.cpp
+++ how to read two lines of input separated by space.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 // Studying how two lines of input is being read in two lines as follows 
+// and how while(t--) works
 // 6
 // 12 13 14 15 66 77
 //
@@ -12,6 +13,9 @@ int main() {
   for(int i=0;i<t;i++) {
       cin>>m[i];
       cout<<i<<" "<<m[i]<<endl;
+  }
+  while(t--){
+      cout<<t<<" : "<<m[t]<<endl;
   }
   return 0;
 }
