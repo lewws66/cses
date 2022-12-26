51b2fc8f3f2f107696d2baa5b494316a29dd8b1e
lewws66 <119733039+lewws66@users.noreply.github.com>
2022-12-26 09:55:20 +0800

    Update bit_strings.cpp

--- bit_strings.cpp
+++ bit_strings.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 //problem
 //https://cses.fi/problemset/task/1617/
-//explanation
+//explanations why modulo 10^9 + 7
 //https://japlslounge.com/posts/cses/bit_strings/1.htm
+//https://www.hackerearth.com/practice/notes/abhinav92003/why-output-the-answer-modulo-109-7/
+//
 int main() {
 int n;
 int s=2;
